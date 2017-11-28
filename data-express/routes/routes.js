c9a39b81d50e061a1e165d2be22aa832e7df7697
John <JAuchterlonie@student.neumont.edu>
2017-11-28 12:27:33 -0700

    .

--- data-express/routes/routes.js
+++ data-express/routes/routes.js
@@ -9,88 +9,86 @@ mdb.once('open', function (callback) {
 });
 
 var userSchema = mongoose.Schema({
-    userName: String,
-    password: String,
-    userLevel: String,
-    email: String,
-    age: String,
-    answer1: String,
-    answer2: String,
-    answer3: String
+  userName: String,
+  password: String,
+  userLevel: String,
+  email: String,
+  age: String,
+  answer1: String,
+  answer2: String,
+  answer3: String
+});
+
+
+var User = mongoose.model('User_Collection', userSchema);
+
+exports.manage = function (req, res) {
+  User.find(function (err, user) {
+    if (err) return console.error(err);
+    res.render('index', {
+      title: 'People List',
+      people: user
+    });
   });
-  
-  
-  var User = mongoose.model('User_Collection', userSchema);
-  
-  
-  
-  exports.manage = function (req, res) {
-    User.find(function (err, user) {
-      if (err) return console.error(err);
-      res.render('index', {
-        title: 'People List',
-        people: user
-      });
+};
+
+exports.create = function (req, res) {
+  res.render('create', {
+      title: 'Add Person'
+  });
+};
+
+exports.createPerson = function (req, res) {
+  var person = new Person({
+    name: req.body.name,
+    age: req.body.age,
+    species: req.body.species
+  });
+  person.save(function (err, person) {
+    if (err) return console.error(err);
+    console.log(req.body.name + ' added');
+  });
+  res.redirect('/');
+};
+
+exports.edit = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    res.render('edit', {
+      title: 'Edit Person',
+      person: person
     });
-  };
-  
-  exports.create = function (req, res) {
-    res.render('create', {
-        title: 'Add Person'
-    });
-  };
-  
-  exports.createPerson = function (req, res) {
-    var person = new Person({
-      name: req.body.name,
-      age: req.body.age,
-      species: req.body.species
-    });
+  });
+};
+
+exports.editPerson = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    person.name = req.body.name;
+    person.age = req.body.age;
+    person.species = req.body.species;
     person.save(function (err, person) {
       if (err) return console.error(err);
-      console.log(req.body.name + ' added');
+      console.log(req.body.name + ' updated');
     });
+  });
+  res.redirect('/');
+
+};
+
+exports.delete = function (req, res) {
+  Person.findByIdAndRemove(req.params.id, function (err, person) {
+    if (err) return console.error(err);
     res.redirect('/');
-  };
-  
-  exports.edit = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-      if (err) return console.error(err);
-      res.render('edit', {
-        title: 'Edit Person',
-        person: person
-      });
+  });
+};
+
+exports.details = function (req, res) {
+  Person.findById(req.params.id, function (err, person) {
+    if (err) return console.error(err);
+    res.render('details', {
+      title: person.name + "'s Details",
+      person: person
     });
-  };
-  
-  exports.editPerson = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-      if (err) return console.error(err);
-      person.name = req.body.name;
-      person.age = req.body.age;
-      person.species = req.body.species;
-      person.save(function (err, person) {
-        if (err) return console.error(err);
-        console.log(req.body.name + ' updated');
-      });
-    });
-    res.redirect('/');
-  
-  };
-  
-  exports.delete = function (req, res) {
-    Person.findByIdAndRemove(req.params.id, function (err, person) {
-      if (err) return console.error(err);
-      res.redirect('/');
-    });
-  };
-  
-  exports.details = function (req, res) {
-    Person.findById(req.params.id, function (err, person) {
-      if (err) return console.error(err);
-      res.render('details', {
-        title: person.name + "'s Details",
-        person: person
-      });
-    });
-  };
+  });
+};
