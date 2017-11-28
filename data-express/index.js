f4d5140feef9150e91bb47d1e8941297ab26922f
Adam Hunting <adam.hunting@gmail.com>
2017-11-28 12:35:03 -0700

    Added to index.js

--- data-express/index.js
+++ data-express/index.js
@@ -8,3 +8,22 @@ bodyParser = require('body-parser');
 
 
 var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+})
+
+app.get('/', route.index);
+app.get('/create', route.create);
+app.get('/edit/:id', route.edit);
+app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createPerson);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/delete/:id', route.delete);
+
+app.listen(3000);
